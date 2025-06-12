733505b68a93c302841ea1dd28896a20c724706a
Rahmade271 <rahmatangga73@gmail.com>
2025-06-12 12:28:09 +0700

    Ragam Batik App

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -2,7 +2,6 @@ const NotFound = {
   async render() {
     return `
       <section id="notFoundPage" class="page-section" style="text-align: center; padding: 50px;">
-        <h2>404 - Halaman Tidak Ditemukan</h2>
         <p>Maaf, halaman yang Anda cari tidak ada.</p>
         <a href="#/" class="primary-button">Kembali ke Beranda</a>
       </section>
